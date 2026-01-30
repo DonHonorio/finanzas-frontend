982976cae12f31a9accbb2b1a70d18fc9d79aec8
Honorio Conesa Acosta <2918257@alu.murciaeduca.es>
2026-01-30 18:48:34 +0100

    cambio de jwt para acceder a la API desplegada

--- app/api/dashboard/[type]/[year]/route.ts
+++ app/api/dashboard/[type]/[year]/route.ts
@@ -1,7 +1,7 @@
 export async function GET(request: Request, { params }: { params: Promise<{ type: string; year: string }> }) {
     const { type, year } = await params
 
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJpYXQiOjE3Njk2MjUwMDMsImV4cCI6MTc3MjIxNzAwM30.PdzbFXbMGHgiWAlNWqu9JXhOQb2MMWHZQqHu4i6-2Hw'
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJpYXQiOjE3Njk3OTUyNDUsImV4cCI6MTc3MjM4NzI0NX0.FAiRLJ-SuyDIJM41Iv-gDjRCsf8zX0UJ4ZCdnKSM2kI'
 
     const url = `${process.env.API_URL}/categories/dashboard/?type=${type}&year=${year}`
     const req = await fetch(url, {
