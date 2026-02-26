b0fd72b44aa06d394038a948657aece182126269
Honorio Conesa Acosta <2918257@alu.murciaeduca.es>
2026-02-26 13:24:35 +0100

    modificado next.config.ts para arreglar error al desplegar máquina en production

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   /* config options here */
   distDir: '.next', // Fuerza que .next se genere en el directorio actual (frontend/)
 }
