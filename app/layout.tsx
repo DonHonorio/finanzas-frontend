fd9ba9c186e19f5f37dd6f94c2d42f76fd933b7e
Honorio Conesa Acosta <2918257@alu.murciaeduca.es>
2026-02-28 22:37:31 +0100

    Probando solución error despliegue: importado css de react-toastify en componente cliente

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import "./globals.css"
+import 'react-toastify/dist/ReactToastify.css'
 import { Outfit } from 'next/font/google'
 import { NextIntlClientProvider } from "next-intl"
 import { getLocale, getMessages } from "next-intl/server"
--- src/components/ui/ToastNotification.tsx
+++ src/components/ui/ToastNotification.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { ToastContainer } from 'react-toastify'
-import "react-toastify/ReactToastify.css"
 
 // Componente wrapper para las notificaciones toast
 // Se importa en el layout de vista mensual para que esté disponible en la vista de la tabla
